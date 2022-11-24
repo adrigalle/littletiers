c9d8d917fa453bae89f9a3182fd3c3a5aa9555f8
adrigalle <adriana.go1105@gmail.com>
2022-11-23 16:58:31 -0800

    added filter, update published, delete entry

--- http_server.js
+++ http_server.js
@@ -1,7 +1,9 @@
 // add http server
 // -----------------------
-// YOUR CODE
+const express = require('express');
+const app = express(); // app is convention
 
+// in larger examples might use const instead
 var low     = require('lowdb');
 var fs      = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
@@ -17,17 +19,39 @@ db.defaults({ posts: []}).write();
 // list posts
 app.get('/data', function(req, res){     
 
-    // YOUR CODE
+    res.send(db.get('posts').value()); // this is simply to see all the posts, will show up on localhost:3000/data, this is basically a read
 
 });
 
 // ----------------------------------------------------
+// This is basically ping=title 1=id published=false
 // add post - test using:
 //      curl http://localhost:3000/posts/ping/1/false
+    //             URL           route title id  published
 // ----------------------------------------------------
 app.get('/posts/:title/:id/:published', function(req, res){
 
-    // YOUR CODE
+    // this makes the :published thingy an actual boolean
+    const isPublished = req.params.published;
+    const isPublishedBoolean = isPublished === 'true' ? true : false;
+
+    const id = parseInt(req.params.id);
+
+    // this is an object for a post
+    var post = {
+        'id': id, // adding the parse makes it a number
+        'title': req.params.title,
+        'published': isPublishedBoolean,
+    }
+    // get a handle on our posts, 
+    // and then push a single post
+    // and then write/save it
+    db.get('posts').push(post).write();
+    // print out to console log (just read)
+    console.log(db.get('posts').value());
+
+    // respond to client and send it back as well to make sure that it is also working there
+    res.send(db.get('posts').value());
 
 });
 
@@ -35,9 +59,22 @@ app.get('/posts/:title/:id/:published', function(req, res){
 // filter by published state - test using:
 //      curl http://localhost:3000/published/true
 // ----------------------------------------------------
+
 app.get('/published/:boolean', function(req, res){
 
-    // YOUR CODE
+    // this makes the :boolean thingy an actual boolean
+    const isPublished = req.params.boolean;
+    const isPublishedBoolean = isPublished === 'true' ? true : false;
+
+    // still have some issues because some are not 100% boolean but close enough
+    const posts = db.get('posts').value();
+    console.log(posts);
+    // filter out any objects with the same published value
+    const published = posts.filter(post => post.published === isPublishedBoolean);
+    // print out on console
+    console.log(published);
+    // respond to client
+    res.send(published);
 
 });
 
@@ -47,7 +84,24 @@ app.get('/published/:boolean', function(req, res){
 // ----------------------------------------------------
 app.get('/published/:id/:boolean', function(req, res){
 
-    // YOUR CODE
+    // this makes the :boolean thingy an actual boolean
+    const isPublished = req.params.boolean;
+    const isPublishedBoolean = isPublished === 'true' ? true : false;
+
+    const id = parseInt(req.params.id);
+    const posts = db.get('posts').value();
+    const index = posts.findIndex(post => post.id == id);
+
+    // make a new little one with the object at posts index
+    const temp = posts[index];
+    // change the boolean thing
+    temp.published = isPublishedBoolean;
+    console.log(temp);
+    // splice the original and put in temp with new boolean value
+    db.get('posts').splice(index, 1, temp).write();
+
+    console.log(db.get('posts').value());
+    res.send(db.get('posts').value());
 
 });
 
@@ -57,10 +111,35 @@ app.get('/published/:id/:boolean', function(req, res){
 // ----------------------------------------------------
 app.get('/delete/:id/', function(req, res){
 
-    // YOUR CODE
+    const id = parseInt(req.params.id);
+    const posts = db.get('posts').value();
+
+    console.log(id);
+    // this finds the index of the post using the id from req
+    const index = posts.findIndex(post => post.id == id);
+    // if not there, it just deletes the last entry
+    console.log(index);
+    db.get('posts').splice(index, 1).write();
+
+
+    console.log(db.get('posts').value());
+    res.send(db.get('posts').value());
 
 });
 
+// ----------------------------------------------------
+// delete All entries - test using:
+//      curl http://localhost:3001/deleteAll
+// ----------------------------------------------------
+app.get("/deleteAll", function (req, res) {
+    // YOUR CODE
+    db.get("posts").remove().write();
+    // 204 Success No Content
+    res.status(204).send();
+  });
+
 // start server
 // -----------------------
-// YOUR CODE
+app.listen(3000, function(){
+    console.log('Running on port 3000!');
+})
--- simple.js
+++ simple.js
@@ -1,19 +1,34 @@
+// some boiler plate for the lowdb
 var low     = require('lowdb');
 var fs      = require('lowdb/adapters/FileSync');
 var adapter = new fs('db.json');
 var db      = low(adapter);
 
-// init the data store
-// ---------------------------
-// YOUR CODE
 
-// add post
-// ----------------------------
-// YOUR CODE
+// set some defaults, this init the data store, this sets array of posts
+db.defaults({ posts: [] })
+    .write()
+
+// add a post (This can probably be one line but I like like this)
+// this pushes entry to posts
+// db.get('posts') // comment out since already ran it, and it would add again
+//     .push({ id: 1, title: 'lowdb is awesome', published: true })
+//     .write()
+// db.get('posts') 
+//     .push({ id: 2, title: 'great', published: false })
+//     .write()
+// db.get('posts') 
+//     .push({ id: 3, title: 'amazing', published: false })
+//     .write()
+// db.get('posts') 
+//     .push({ id: 4, title: 'rando', published: true })
+//     .write()
+
+// found in the documentation, to print all the ones that were already persisted
+console.log(db.get('posts').value());
 
 // count posts
-// ----------------------------
-// YOUR CODE
+console.log(db.data);
 
 // find all posts ids
 // ----------------------------
